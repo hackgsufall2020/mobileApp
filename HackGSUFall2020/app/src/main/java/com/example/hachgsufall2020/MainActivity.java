65b2f0dbad6a4c3a972e3275fdb1c0825e4f5e8b
Landon Wang <landonwang558@gmail.com>
2020-11-21 21:18:54 -0500

    Update @ 9:18
    removed old file, posted most recent files

---
+++ HackGSUFall2020/app/src/main/java/com/example/hachgsufall2020/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hachgsufall2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button regButton;
+    private Button signIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
